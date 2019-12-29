82d617aa25c6a53b45eb48c8cd4f976fce7e9a32
Thomas Kolonko <tz.kolonko@gmail.com>
2019-12-29 18:12:44 +0100

    first version of the CourtCount2

--- app/src/main/java/com/example/android/courtcount2/MainActivity.java
+++ app/src/main/java/com/example/android/courtcount2/MainActivity.java
@@ -3,12 +3,71 @@ package com.example.android.courtcount2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team A
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void displayThreePointsA(View view) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void displayTwoPointsA(View view) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void displayOnePointA(View view) {
+        scoreTeamA++;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void displayThreePointsB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void displayTwoPointsB(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void displayOnePointB(View view) {
+        scoreTeamB++;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void reset(View view) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 }
